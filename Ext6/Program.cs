c4fb77f29c5916f31c323ff5201cb4552368c5da
Evgenii <zvolev@mail.ru>
2022-10-05 22:30:05 +0300

    заполнение и печать массива рандомно

---
+++ Ext6/Program.cs
@@ -0,0 +1,27 @@
+﻿// заполнение и печать массива рандомно
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 50);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[25];
+
+FillArray(array);
+PrintArray(array);
