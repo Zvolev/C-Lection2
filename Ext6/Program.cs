d5351f6253e015b07e7bffccaa16f9221495f955
Evgenii <zvolev@mail.ru>
2022-10-05 23:16:12 +0300

    поиск позиции массива по первому совпадению значения

--- Ext6/Program.cs
+++ Ext6/Program.cs
@@ -1,11 +1,11 @@
-﻿// заполнение и печать массива рандомно
+﻿// поиск позиции массива по первому совпадению значения
 void FillArray(int[] collection)
 {
     int length = collection.Length;
     int index = 0;
     while (index < length)
     {
-        collection[index] = new Random().Next(1, 50);
+        collection[index] = new Random().Next(1, 10);
         index++;
     }
 }
@@ -21,7 +21,29 @@ void PrintArray(int[] col)
     }
 }
 
-int[] array = new int[25];
+int IndexOf(int[] colection, int find)
+{
+    int count = colection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)   
+    {
+        if (colection[index] == find)
+        {
+          position = index;
+          break;  
+        } 
+        index++;
+    }
+    return position;    
+}
+
+int[] array = new int[10];
 
 FillArray(array);
 PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
